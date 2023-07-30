b63579d6d472fbe4451d68148b37ffccac384b76
pae <Andrew.p52@yandex.ru>
2023-07-31 00:01:59 +0300

    Pr start

---
+++ Back/MarketBackend/src/test/java/com/pashenko/marketbackend/MarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pashenko.marketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

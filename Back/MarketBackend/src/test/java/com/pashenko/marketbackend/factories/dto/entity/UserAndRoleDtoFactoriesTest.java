4dacb6e7dc48ec6726b2c35aa337f150dc71c84f
pae <Andrew.p52@yandex.ru>
2023-08-15 23:29:51 +0300

    User&Role dto factories tests

---
+++ Back/MarketBackend/src/test/java/com/pashenko/marketbackend/factories/dto/entity/UserAndRoleDtoFactoriesTest.java
@@ -0,0 +1,63 @@
+package com.pashenko.marketbackend.factories.dto.entity;
+
+import com.pashenko.marketbackend.dto.RoleDto;
+import com.pashenko.marketbackend.dto.UserDto;
+import com.pashenko.marketbackend.entities.Role;
+import com.pashenko.marketbackend.entities.User;
+import com.pashenko.marketbackend.factories.dto.ExtendedDtoFactory;
+import com.pashenko.marketbackend.factories.dto.RoleDtoFactory;
+import com.pashenko.marketbackend.factories.dto.UserDtoFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public class UserAndRoleDtoFactoriesTest {
+    private final RoleDtoFactory roleDtoFactory = new RoleDtoFactory();
+    private final UserDtoFactory userDtoFactory = new UserDtoFactory(roleDtoFactory);
+    private static final Role roleEntity = new Role();
+    private static final User userEntity = new User();
+
+    @BeforeAll
+    public static void initEntities(){
+        roleEntity.setRole("USER");
+        roleEntity.setId(1L);
+        roleEntity.setCreated(LocalDateTime.now());
+        roleEntity.setModified(LocalDateTime.now());
+
+        userEntity.setEnabled(true);
+        userEntity.setUsername("test");
+        userEntity.setPassword("test");
+        userEntity.setId(1L);
+        userEntity.setRoles(Set.of(roleEntity));
+        userEntity.setCreated(LocalDateTime.now());
+        userEntity.setModified(LocalDateTime.now());
+    }
+
+    @Test
+    public void roleDtoFactoryReturnsCorrectSimpleDto(){
+        RoleDto dto = roleDtoFactory.getDto(roleEntity);
+        assertEquals(roleEntity.getRole(), dto.getRole());
+        assertEquals(roleEntity.getId(), dto.getId());
+        assertNull(dto.getCreated());
+        assertNull(dto.getModified());
+    }
+
+    @Test
+    public void userDtoFactoryReturnsCorrectSimpleDto(){
+        UserDto dto = userDtoFactory.getDto(userEntity);
+        assertEquals(userEntity.getId(), dto.getId());
+        assertEquals(userEntity.getUsername(), dto.getUsername());
+        assertEquals(userEntity.getCreated(), dto.getCreated());
+        assertEquals(userEntity.getModified(), dto.getModified());
+        assertNull(dto.getPassword());
+    }
+
+    @Test
+    public void userDtoFactoryReturnsCorrectExtendedDto(){
+        UserDto dto = userDtoFactory.getExtendedDto(userEntity);
+        assertNotNull(dto.getRoles());
+        assertEquals(1, dto.getRoles().size());
+    }
+}

166b455afcf7f93d8fcce3e8cabd30a61f5d3b8b
pae <Andrew.p52@yandex.ru>
2023-08-15 23:27:51 +0300

    Auth test

--- Back/MarketBackend/src/main/java/com/pashenko/marketbackend/dto/LoginRequest.java
+++ Back/MarketBackend/src/main/java/com/pashenko/marketbackend/dto/LoginRequest.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     private String username;
     private String password;
---
+++ Back/MarketBackend/src/test/java/com/pashenko/marketbackend/PrimaryAuthenticationTest.java
@@ -0,0 +1,58 @@
+package com.pashenko.marketbackend;
+
+import com.pashenko.marketbackend.dto.LoginRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+/**
+ * Primary authentication test with existing user (no mocks)
+ */
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class PrimaryAuthenticationTest {
+    private final String LOGIN_URL = "/users/signin";
+    private final String TOKEN_PREFIX = "apiToken";
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private LoginRequest correct = new LoginRequest("user", "user");
+    private LoginRequest incorrect = new LoginRequest("user", "u");
+
+
+    @Test
+    public void authRequestWithCorrectCredsWillSucceedWith200() throws Exception {
+        ResponseEntity<String> result = restTemplate
+                .postForEntity(LOGIN_URL, correct, String.class);
+        Assertions.assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    public void authRequestWithCorrectCredsWillSetCookieWithJwt() throws Exception {
+        ResponseEntity<String> result = restTemplate
+                .postForEntity(LOGIN_URL, correct, String.class);
+        List<String> cookies = result.getHeaders().get("Set-Cookie");
+        Assertions.assertNotNull(cookies);
+        Assertions.assertTrue(cookies.stream().anyMatch(s -> s.contains(TOKEN_PREFIX)));
+    }
+
+    @Test
+    public void authRequestWithIncorrectCredsWillFailedWith403() throws Exception {
+        ResponseEntity<String> result = restTemplate
+                .postForEntity(LOGIN_URL, incorrect, String.class);
+        Assertions.assertEquals(HttpStatus.FORBIDDEN, result.getStatusCode());
+    }
+
+
+}
